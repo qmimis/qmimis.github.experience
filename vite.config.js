8f2fb2b2c99bdbae13cb9f7f1c684df3426689b5
qmimis <165008818+qmimis@users.noreply.github.com>
2026-04-16 17:46:21 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,9 @@ import VitePluginMarkdown from 'vite-plugin-md'
 
 export default defineConfig({
   lang: 'zh-CN',
-<<<<<<< HEAD
   title: 'qmimis的个人博客',
   base:'/qmimis/qmimis.github.experience/',
-=======
   title: '我的个人博客',
-
->>>>>>> 4f3fa74b050d71327a6f85eebf800c4678289768
   plugins: [
     vue({
       include: [/\.vue$/, /\.md$/], // 让Vue插件也编译.md文件
