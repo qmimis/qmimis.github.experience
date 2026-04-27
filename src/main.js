2ed8eb911dcb87fde518fa23abade80d32ee281b
qmimis <165008818+qmimis@users.noreply.github.com>
2026-04-27 15:08:02 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { createApp } from 'vue'
+import { createApp } from './node_modules/vue/dist/vue.esm-browser.js'
 import App from './App.vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
